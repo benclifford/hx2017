96889eb109c329807b76d06f8017aa4ba8aec53a
Ben Clifford <benc@hawaga.org.uk>
2017-10-11 16:58:09 +0100

    Append eof automatically so that people don't have to

--- lib/Lib.hs
+++ lib/Lib.hs
@@ -7,14 +7,14 @@ import Text.Trifecta
 --   the output.
 p :: Parser t -> String -> t
 p parser input =
-  let res = parseString parser mempty input
+  let res = parseString (parser <* eof) mempty input
   in case res of
     Failure f -> error (show (_errDoc f))
     Success n -> n
 
 pfails :: Parser t -> String -> ()
 pfails parser input =
-  case (parseString parser mempty input)
+  case (parseString (parser <* eof) mempty input)
    of
     Failure f -> ()
     Success n -> error "This parser was supposed to fail"
